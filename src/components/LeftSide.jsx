f3c6f4f0a090707a1b000bed1f0d277d1da1f76a
Fsk196 <kshoeb196@gmail.com>
2024-09-07 18:13:20 +0530

    Deployment issue fix

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -4,7 +4,7 @@ const LeftSide = () => {
   return (
     <>
       <div className="w-[836px] h-[689px] bg-[#616161D1]/80 rounded-[27px] shadow-xl hidden lg:flex justify-center items-center">
-        <h2 className="w-2/3 text-gray-200 text-lg ">This website is only for laptop view as mentioned in the assignment below 768px i wont be that responsive. I majorly kept it responsive for above 768px </h2>
+        <h2 className="w-2/3 text-gray-200 text-lg ">This website is only for laptop view as mentioned in the assignment below 768px it wont be that responsive. I majorly kept it responsive for above 768px </h2>
       </div>
     </>
   );
